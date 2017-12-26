6da7b020cab74e2ddb298725d203d024da787afa
ChenJoe <zeichenjoyce@gmail.com>
2017-12-26 15:47:11 +0800

    commit

---
+++ public/app.js
@@ -0,0 +1,292 @@
+var OV;
+var session;
+
+var sessionId;
+var token;
+var nickName;
+var userName;
+var sessionName;
+
+
+/* OPENVIDU METHODS */
+
+function joinSession() {
+	getSessionIdAndToken(function () {
+
+		// --- 1) Get an OpenVidu object and init a session with the retrieved sessionId ---
+
+		OV = new OpenVidu();
+		session = OV.initSession(sessionId);
+
+
+		// --- 2) Specify the actions when events take place ---
+
+		// On every new Stream received...
+		session.on('streamCreated', function (event) {
+
+			// Subscribe to the Stream to receive it
+			// HTML video will be appended to element with 'video-container' id
+			var subscriber = session.subscribe(event.stream, 'video-container');
+			
+			// When the HTML video has been appended to DOM...
+			subscriber.on('videoElementCreated', function (event) {
+			
+				// Add a new <p> element for the user's name and nickname just below its video
+				appendUserData(event.element, subscriber.stream.connection);
+			});
+		});
+
+		// On every Stream destroyed...
+		session.on('streamDestroyed', function (event) {
+			// Delete the HTML element with the user's name and nickname
+			removeUserData(event.stream.connection);
+		});
+
+
+		// --- 3) Connect to the session passing the retrieved token and some more data from
+		//         the client (in this case a JSON with the nickname chosen by the user) ---
+
+		session.connect(token, '{"clientData": "' + nickName + '"}', function (error) {
+
+			// If the connection is successful, initialize a publisher and publish to the session
+			if (!error) {
+
+				// Here we check somehow if the user has at least 'PUBLISHER' role before
+				// trying to publish its stream. Even if someone modified the client's code and
+				// published the stream, it wouldn't work if the token sent in Session.connect
+				// method doesn't belong to a 'PUBLIHSER' role
+				if (isPublisher()) {
+
+					// --- 4) Get your own camera stream ---
+
+					var publisher = OV.initPublisher('video-container', {
+						audio: true,        // Whether you want to transmit audio or not
+						video: true,        // Whether you want to transmit video or not
+						audioActive: true,  // Whether you want to start the publishing with your audio unmuted or muted
+						videoActive: true,  // Whether you want to start the publishing with your video enabled or disabled
+						quality: 'MEDIUM',  // The quality of your video ('LOW', 'MEDIUM', 'HIGH')
+						screen: false       // true to get your screen as video source instead of your camera
+					});
+
+					// When our HTML video has been added to DOM...
+					publisher.on('videoElementCreated', function (event) {
+						// Init the main video with ours and append our data
+						var userData = {
+							nickName: nickName,
+							userName: userName
+						};
+						initMainVideo(event.element, userData);
+						appendUserData(event.element, userData);
+						$(event.element).prop('muted', true);  // Mute lcoal video
+					});
+
+
+					// --- 5) Publish your stream ---
+
+					session.publish(publisher);
+
+				} else {
+					console.warn('You don\'t have permissions to publish');
+					initMainVideoThumbnail(); // Show SUBSCRIBER message in main video
+				}
+			} else {
+				console.warn('There was an error connecting to the session:', error.code, error.message);
+			}
+		});
+
+		$('#session-title').text(sessionName);
+		$('#join').hide();
+		$('#session').show();
+
+		return false;
+	});
+}
+
+function leaveSession() {
+
+	// 6) Leave the session by calling 'disconnect' method over the Session object
+
+	session.disconnect();
+	session = null;
+
+	// Removing all HTML elements with the user's nicknames
+	cleanSessionView();
+
+	$('#join').show();
+	$('#session').hide();
+}
+
+/* OPENVIDU METHODS */
+
+
+
+/* APPLICATION REST METHODS */
+
+function logIn() {
+	var user = $("#user").val(); // Username
+	var pass = $("#pass").val(); // Password
+	var jsonBody = JSON.stringify({ // Body of POST request
+		'user': user,
+		'pass': pass
+	});
+
+	userName = user;
+
+	httpRequest('POST', '/api-login/login', jsonBody, 'Login WRONG', function successCallback(response){ // Send POST request
+		console.warn(userName + ' login');
+		$("#name-user").text(user);
+		$("#not-logged").hide();
+		$("#logged").show();
+		// Random nickName and session
+		$("#sessionName").val("Session " + Math.floor(Math.random() * 10));
+		$("#participantName").val("Participant " + Math.floor(Math.random() * 100));
+	});
+}
+
+function logOut() {
+	httpRequest('GET', 'api-login/logout', null, 'Logout WRONG', function successCallback(response) {
+		console.warn(userName + ' logout');
+		$("#not-logged").show();
+		$("#logged").hide();
+	});
+}
+
+function getSessionIdAndToken(callback) {
+	sessionName = $("#sessionName").val(); // Video-call chosen by the user
+	nickName = $("#participantName").val(); // Nickname chosen by the user
+	var jsonBody = JSON.stringify({ // Body of POST request
+		'session': sessionName
+	});
+
+	// Send POST request
+	httpRequest('POST', 'api-sessions/get-sessionid-token', jsonBody, 'Request of SESSIONID and TOKEN gone WRONG:', function successCallback(response) {
+		sessionId = response[0]; // Get sessionId from response
+		token = response[1]; // Get token from response
+		console.warn('Request of SESSIONID and TOKEN gone WELL (SESSIONID:' + sessionId + ", TOKEN:" + token + ")");
+		callback(); // Continue the join operation
+	});
+}
+
+function removeUser() {
+	// Body of POST request with the name of the session and the token of the leaving user
+	var jsonBody = JSON.stringify({
+		'sessionName': sessionName,
+		'token': token
+	});
+
+	// Send POST request
+	httpRequest('POST', 'api-sessions/remove-user', jsonBody, 'User couldn\'t be removed from session', function successCallback(response) {
+		console.warn(userName + " correctly removed from session");
+	});
+}
+
+function httpRequest(method, url, body, errorMsg, callback) {
+	var http = new XMLHttpRequest();
+	http.open(method, url, true);
+	http.setRequestHeader('Content-type', 'application/json');
+	http.addEventListener('readystatechange', processRequest, false);
+	http.send(body);
+
+	function processRequest() {
+		if (http.readyState == 4) {
+			if (http.status == 200) {
+				try {
+					callback(JSON.parse(http.responseText));
+				} catch (e) {
+					callback();
+				}
+			} else {
+				console.warn(errorMsg);
+				console.warn(http.responseText);
+			}
+		}
+	}
+}
+
+/* APPLICATION REST METHODS */
+
+
+
+/* APPLICATION BROWSER METHODS */
+
+window.onbeforeunload = function () { // Gracefully leave session
+	if (session) {
+		removeUser();
+		leaveSession();
+	}
+	logOut();
+}
+
+function appendUserData(videoElement, connection) {
+	var clientData;
+	var serverData;
+	var nodeId;
+	if (connection.nickName) { // Appending local video data
+		clientData = connection.nickName;
+		serverData = connection.userName;
+		nodeId = 'main-videodata';
+	} else {
+		clientData = JSON.parse(connection.data.split('%/%')[0]).clientData;
+		serverData = JSON.parse(connection.data.split('%/%')[1]).serverData;
+		nodeId = connection.connectionId;
+	}
+	var dataNode = document.createElement('div');
+	dataNode.className = "data-node";
+	dataNode.id = "data-" + nodeId;
+	dataNode.innerHTML = "<p class='nickName'>" + clientData + "</p><p class='userName'>" + serverData + "</p>";
+	videoElement.parentNode.insertBefore(dataNode, videoElement.nextSibling);
+	addClickListener(videoElement, clientData, serverData);
+}
+
+function removeUserData(connection) {
+	var userNameRemoved = $("#data-" + connection.connectionId);
+	if ($(userNameRemoved).find('p.userName').html() === $('#main-video p.userName').html()) {
+		cleanMainVideo(); // The participant focused in the main video has left
+	}
+	$("#data-" + connection.connectionId).remove();
+}
+
+function removeAllUserData() {
+	$(".data-node").remove();
+}
+
+function cleanMainVideo() {
+	$('#main-video video').get(0).srcObject = null;
+	$('#main-video p').each(function () {
+		$(this).html('');
+	});
+}
+
+function addClickListener(videoElement, clientData, serverData) {
+	videoElement.addEventListener('click', function () {
+		var mainVideo = $('#main-video video').get(0);
+		if (mainVideo.srcObject !== videoElement.srcObject) {
+			$('#main-video p.nickName').html(clientData);
+			$('#main-video p.userName').html(serverData);
+			mainVideo.srcObject = videoElement.srcObject;
+		}
+	});
+}
+
+function initMainVideo(videoElement, userData) {
+	$('#main-video video').get(0).srcObject = videoElement.srcObject;
+	$('#main-video p.nickName').html(userData.nickName);
+	$('#main-video p.userName').html(userData.userName);
+	$('#main-video video').prop('muted', true);
+}
+
+function initMainVideoThumbnail() {
+	$('#main-video video').css("background", "url('images/subscriber-msg.jpg') round");
+}
+
+function isPublisher() {
+	return userName.includes('publisher');
+}
+
+function cleanSessionView() {
+	removeAllUserData();
+	cleanMainVideo();
+	$('#main-video video').css("background", "");
+}
+
+/* APPLICATION BROWSER METHODS */
---
+++ server.js
@@ -0,0 +1,261 @@
+/* CONFIGURATION */
+
+var OpenVidu = require('openvidu-node-client').OpenVidu;
+var Session = require('openvidu-node-client').Session;
+var OpenViduRole = require('openvidu-node-client').OpenViduRole;
+var TokenOptions = require('openvidu-node-client').TokenOptions;
+
+// Check launch arguments: must receive openvidu-server URL and the secret
+if (process.argv.length != 4) {
+    console.log("Usage: node " + __filename + " OPENVIDU_URL OPENVIDU_SECRET");
+    process.exit(-1);
+}
+// For demo purposes we ignore self-signed certificate
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
+
+// Node imports
+var express = require('express');
+var fs = require('fs');
+var session = require('express-session');
+var https = require('https');
+var bodyParser = require('body-parser'); // Pull information from HTML POST (express4)
+var app = express(); // Create our app with express
+
+// Server configuration
+app.use(session({
+    saveUninitialized: true,
+    resave: false,
+    secret: 'MY_SECRET'
+}));
+app.use(express.static(__dirname + '/public')); // Set the static files location
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+})); // Parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // Parse application/json
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+})); // Parse application/vnd.api+json as json
+
+// Listen (start app with node server.js)
+var options = {
+    key: fs.readFileSync('openvidukey.pem'),
+    cert: fs.readFileSync('openviducert.pem')
+};
+https.createServer(options, app).listen(5000);
+
+// Mock database
+var users = [{
+    user: "publisher1",
+    pass: "pass",
+    role: OpenViduRole.PUBLISHER
+}, {
+    user: "publisher2",
+    pass: "pass",
+    role: OpenViduRole.PUBLISHER
+}, {
+    user: "subscriber",
+    pass: "pass",
+    role: OpenViduRole.SUBSCRIBER
+}];
+
+// Environment variable: URL where our OpenVidu server is listening
+var OPENVIDU_URL = process.argv[2];
+// Environment variable: secret shared with our OpenVidu server
+var OPENVIDU_SECRET = process.argv[3];
+
+// OpenVidu object to ask openvidu-server for sessionId and token
+var OV = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+
+// Collection to pair session names and OpenVidu Session objects
+var mapSessionNameSession = {};
+// Collection to pair sessionId's (identifiers of Session objects) and tokens
+var mapSessionIdTokens = {};
+
+console.log("App listening on port 5000");
+
+/* CONFIGURATION */
+
+
+
+/* REST API */
+
+// Login
+app.post('/api-login/login', function (req, res) {
+    
+    // Retrieve params from POST body
+    var user = req.body.user;
+    var pass = req.body.pass;
+    console.log("Logging in | {user, pass}={" + user + ", " + pass + "}");
+
+    if (login(user, pass)) { // Correct user-pass
+        // Validate session and return OK 
+        // Value stored in req.session allows us to identify the user in future requests
+        console.log("'" + user + "' has logged in");
+        req.session.loggedUser = user;
+        res.status(200).send();
+    } else { // Wrong user-pass
+        // Invalidate session and return error
+        console.log("'" + user + "' invalid credentials");
+        req.session.destroy();
+        res.status(401).send('User/Pass incorrect');
+    }
+});
+
+// Logout
+app.get('/api-login/logout', function (req, res) {
+    console.log("'" + req.session.loggedUser + "' has logged out");
+    req.session.destroy();
+    res.status(200).send();
+});
+
+// Get sessionId and token (add new user to session)
+app.post('/api-sessions/get-sessionid-token', function (req, res) {
+    if (!isLogged(req.session)) {
+        req.session.destroy();
+        res.status(401).send('User not logged');
+    } else {
+        // The video-call to connect ("TUTORIAL")
+        var sessionName = req.body.session;
+    
+        // Role associated to this user
+        var role = users.find(u => (u.user === req.session.loggedUser)).role;
+    
+        // Optional data to be passed to other users when this user connects to the video-call
+        // In this case, a JSON with the value we stored in the req.session object on login
+        var serverData = '{"serverData": "' + req.session.loggedUser + '"}';
+
+        console.log("Getting sessionId and token | {sessionName}={" + sessionName + "}");
+    
+        // Build tokenOptions object with the serverData and the role
+        var tokenOptions = new TokenOptions.Builder()
+            .data(serverData)
+            .role(role)
+            .build();
+
+        if (mapSessionNameSession[sessionName]) {
+            // Session already exists: return existing sessionId and a new token
+            console.log('Existing session ' + sessionName);
+            
+            // Get the existing Session from the collection
+            var mySession = mapSessionNameSession[sessionName];
+            
+            // Generate a new token asynchronously with the recently created tokenOptions
+            mySession.generateToken(tokenOptions, function (token) {
+                
+                // Get the existing sessionId
+                mySession.getSessionId(function (sessionId) {
+                
+                    // Store the new token in the collection of tokens
+                    mapSessionIdTokens[sessionId].push(token);
+                    
+                    // Return the sessionId and token to the client
+                    console.log('SESSIONID: ' + sessionId);
+                    console.log('TOKEN: ' + token);
+                    res.status(200).send({
+                        0: sessionId,
+                        1: token
+                    });
+                });
+            });
+        } else { // New session: return a new sessionId and a new token
+            console.log('New session ' + sessionName);
+
+            // Create a new OpenVidu Session
+            var mySession = OV.createSession();
+
+            // Get the sessionId asynchronously
+            mySession.getSessionId(function (sessionId) {
+
+                // Store the new Session in the collection of Sessions
+                mapSessionNameSession[sessionName] = mySession;
+                // Store a new empty array in the collection of tokens
+                mapSessionIdTokens[sessionId] = [];
+                
+                // Generate a new token asynchronously with the recently created tokenOptions
+                mySession.generateToken(tokenOptions, function (token) {
+                    
+                    // Store the new token in the collection of tokens
+                    mapSessionIdTokens[sessionId].push(token);
+
+                    console.log('SESSIONID: ' + sessionId);
+                    console.log('TOKEN: ' + token);
+                    
+                    // Return the sessionId and token to the client
+                    res.status(200).send({
+                        0: sessionId,
+                        1: token
+                    });
+                });
+            });
+        }
+    }
+});
+
+// Remove user from session
+app.post('/api-sessions/remove-user', function (req, res) {
+    if (!isLogged(req.session)) {
+        req.session.destroy();
+        res.status(401).send('User not logged');
+    } else {
+        // Retrieve params from POST body
+        var sessionName = req.body.sessionName;
+        var token = req.body.token;
+        console.log('Removing user | {sessionName, token}={' + sessionName + ', ' + token + '}');
+
+        // If the session exists
+        var mySession = mapSessionNameSession[sessionName];
+        if (mySession) {
+            mySession.getSessionId(function (sessionId) {
+                var tokens = mapSessionIdTokens[sessionId];
+                if (tokens) {
+                    var index = tokens.indexOf(token);
+                    
+                    // If the token exists
+                    if (index !== -1) {
+                        // Token removed!
+                        tokens.splice(index, 1);
+                        console.log(sessionName + ': ' + mapSessionIdTokens[sessionId].toString());
+                    } else {
+                        var msg = 'Problems in the app server: the TOKEN wasn\'t valid';
+                        console.log(msg);
+                        res.status(500).send(msg);
+                    }
+                    if (mapSessionIdTokens[sessionId].length == 0) {
+                        // Last user left: session must be removed
+                        console.log(sessionName + ' empty!');
+                        delete mapSessionNameSession[sessionName];
+                    }
+                    res.status(200).send();
+                } else {
+                    var msg = 'Problems in the app server: the SESSIONID wasn\'t valid';
+                    console.log(msg);
+                    res.status(500).send(msg);
+                }
+            });
+        } else {
+            var msg = 'Problems in the app server: the SESSION does not exist';
+            console.log(msg);
+            res.status(500).send(msg);
+        }
+    }
+});
+
+/* REST API */
+
+
+
+/* AUXILIARY METHODS */
+
+function login(user, pass) {
+    return (users.find(u => (u.user === user) && (u.pass === pass)));
+}
+
+function isLogged(session) {
+    return (session.loggedUser != null);
+}
+
+function getBasicAuth() {
+    return 'Basic ' + (new Buffer('OPENVIDUAPP:' + OPENVIDU_SECRET).toString('base64'));
+}
+
+/* AUXILIARY METHODS */
